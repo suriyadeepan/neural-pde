c0de574d8d9e4cd25f162aa852593a335a46d1f7
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2020-01-03 22:38:45 +0530

    tests forward pass : success

---
+++ pinn.py
@@ -0,0 +1,12 @@
+from neuralpde.pinn import PiDiscoveryNet
+from neuralpde import data
+
+
+if __name__ == '__main__':
+  training_set_size = 5000
+  # get data < (training data, bounds, raw mat)
+  df, bounds, nsw = data.navierstokes_wake(N=training_set_size)
+  layers = [3, 20, 20, 20, 20, 20, 20, 20, 20, 2]
+  pidn = PiDiscoveryNet(layers, bounds)
+  u, v, p, f_u, f_v = pidn(1., 1., 1.)
+  print(u[0], v[0], p[0], f_u[0], f_v[0])
